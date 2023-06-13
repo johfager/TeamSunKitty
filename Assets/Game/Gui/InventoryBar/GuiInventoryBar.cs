14dc7eddbe28823e9a70521cc85fe82e67a9f4e3
silverwavetidaldragon <78051214+silverwavetidaldragon@users.noreply.github.com>
2023-06-13 15:59:25 +0200

    updates inventory bar sprite

---
+++ Assets/Game/Gui/InventoryBar/GuiInventoryBar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using PowerTools.Quest;
+using PowerScript;
+using static GlobalScript;
+
+public class GuiInventoryBar : GuiScript<GuiInventoryBar>
+{
+
+
+	void OnShow()
+	{
+	}
+
+	void OnPostRestore( int version )
+	{
+	}
+}
