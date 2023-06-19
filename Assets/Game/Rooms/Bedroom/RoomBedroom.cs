2de5e0fe908281c630bfd277228802d7d8732221
silverwavetidaldragon <78051214+silverwavetidaldragon@users.noreply.github.com>
2023-06-19 18:58:34 +0200

    add newspaper sounds

--- Assets/Game/Rooms/Bedroom/RoomBedroom.cs
+++ Assets/Game/Rooms/Bedroom/RoomBedroom.cs
@@ -271,17 +271,37 @@ public class RoomBedroom : RoomScript<RoomBedroom>
 
 	IEnumerator OnInteractPropOminousPaper( IProp prop )
 	{
+		Audio.Play("Soundnewspaper");
+		
 		yield return C.Display("Long ago, by Storsjön shore");
+		Audio.Play("Soundnewspaper");
+		
 		yield return C.Display("Jata and Kata’s cauldron bore");
+		Audio.Play("Soundnewspaper");
+		
 		yield return C.Display("A boiling brew, trolls carefully tend");
+		Audio.Play("Soundnewspaper");
+		
 		yield return C.Display("Ingredients galore, holding no end");
+		Audio.Play("Soundnewspaper");
+		
 		
 		yield return C.Display("Hark, the cauldron shouted a cry");
+		Audio.Play("Soundnewspaper");
+		
 		yield return C.Display("A serpent, a creature, a cat head held high");
+		Audio.Play("Soundnewspaper");
+		
 		yield return C.Display("It vanished in waters growing in might");
+		Audio.Play("Soundnewspaper");
+		
 		yield return C.Display("Eyes averted, true horror in sight");
+		Audio.Play("Soundnewspaper");
+		
 		
 		yield return C.Display("The poem seems to be cut off here…");
+		Audio.Play("Soundnewspaper");
+		
 		
 		yield return E.Break;
 	}
